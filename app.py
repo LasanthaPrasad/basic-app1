1ea088a7c9eaa5c81019020cea13eca618ac1a43
LasanthaPrasad <ee_prasad@yahoo.com>
2024-08-16 16:31:57 +0530

    first commit

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 from flask import render_template
 from markupsafe import Markup
+from datetime import datetime
+from sqlalchemy import func
 
 
 app = Flask(__name__)
@@ -29,6 +31,28 @@ app.config['GOOGLE_MAPS_API_KEY'] = os.environ.get('GOOGLE_MAPS_API_KEY', 'your_
 
 db = SQLAlchemy(app)
 
+
+class GridSubstation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    code = db.Column(db.String(50), nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    current_load = db.Column(db.Float)
+    solar_plants = db.relationship('SolarPlant', backref='grid_substation', lazy=True)
+
+class SubstationForecast(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    substation_id = db.Column(db.Integer, db.ForeignKey('grid_substation.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    generation_forecast = db.Column(db.Float)
+    load_forecast = db.Column(db.Float)
+
+    substation = db.relationship('GridSubstation', backref=db.backref('forecasts', lazy=True))
+
+
+
+
 class GridSubList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
@@ -46,14 +70,18 @@ class SolarPlant(db.Model):
     grid_substation_id = db.Column(db.Integer, db.ForeignKey('grid_sub_list.id'), nullable=False)
     grid_substation = db.relationship('GridSubList', backref=db.backref('solar_plants', lazy=True))
     connected_feeder = db.Column(db.String(100), nullable=False)
-
-
+    grid_substation_id = db.Column(db.Integer, db.ForeignKey('grid_substation.id'), nullable=False)
 
 
 
 @app.route('/map')
 def map_view():
     plants = SolarPlant.query.all()
+    substations = GridSubstation.query.all()
+    
+    current_time = datetime.utcnow()
+    forecast_end = current_time + timedelta(days=3)
+    
     plants_data = [{
         'id': plant.id,
         'name': plant.name,
@@ -63,12 +91,33 @@ def map_view():
         'angle': plant.angle,
         'max_power': plant.max_power,
         'owner_name': plant.owner_name,
-        'grid_substation': plant.grid_substation.name if plant.grid_substation else '',
+        'grid_substation': plant.grid_substation.name,
         'connected_feeder': plant.connected_feeder
     } for plant in plants]
-    
-    plants_json = json.dumps(plants_data)
-    return render_template('map.html', plants_json=Markup(plants_json))
+
+    substations_data = [{
+        'id': sub.id,
+        'name': sub.name,
+        'code': sub.code,
+        'latitude': sub.latitude,
+        'longitude': sub.longitude,
+        'current_load': sub.current_load,
+        'forecasts': [{
+            'timestamp': f.timestamp.isoformat(),
+            'generation_forecast': f.generation_forecast,
+            'load_forecast': f.load_forecast
+        } for f in SubstationForecast.query.filter(
+            SubstationForecast.substation_id == sub.id,
+            SubstationForecast.timestamp.between(current_time, forecast_end)
+        ).order_by(SubstationForecast.timestamp).all()]
+    } for sub in substations]
+
+    return render_template('map.html', 
+                           plants_json=json.dumps(plants_data),
+                           substations_json=json.dumps(substations_data))
+
+
+
 
 
 
